72fe3f424fe61c7394845cb176ecc395c3193991
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-01 19:49:55 +0900

    Add: next-auth

---
+++ src/app/Card.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  user: string;
+};
+
+export default function Card({ user }: { user: string | null | undefined }) {
+  return (
+    <div className='flex flex-col items-center justify-center gap-2 p-5 max-w-xs w-full bg-white shadow-lg rounded-lg font-bold text-xl'>
+      Welcome Back {user}
+    </div>
+  );
+}
