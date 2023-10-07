32453891af6ec88d39abc69514503239db1f2217
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-07 18:42:59 +0900

    Fix: issuse検証

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,16 @@
-import { options } from './options';
+import type { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
 
-const handler = NextAuth(options);
+export const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+  ],
+};
+
+const handler: NextAuthOptions = NextAuth(options);
 
 export { handler as GET, handler as POST };
