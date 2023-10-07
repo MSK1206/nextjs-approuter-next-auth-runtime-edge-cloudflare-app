c0ace528bca88a5d03495cfb6dcbad054aa4ef5f
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-07 19:23:06 +0900

    Fix: issuse検証

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import Form from './Form';
 import Card from './Card';
 import { getServerSession } from 'next-auth';
-import { options } from './api/auth/[...nextauth]/route';
+import { AUTHCONFIG } from './api/auth/[...nextauth]/route';
 
 export default async function Home() {
-  const session = await getServerSession(options);
+  const session = await getServerSession(AUTHCONFIG);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       {session ? <Card user={session.user?.name} /> : <Form />}
