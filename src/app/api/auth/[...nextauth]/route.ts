8db1f252bdbb4400c7a9b28cb9440de20a540833
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-07 19:51:05 +0900

    Fix: issuse検証おわり

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
+import NextAuth from 'next-auth';
+import { options } from './options';
 
-const Config: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-  ],
-};
+const handler = NextAuth(options);
 
-
-const handler = NextAuth(Config);
-
-export { handler as GET, handler as POST, Config as AUTHCONFIG };
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import Form from './Form';
 import Card from './Card';
 import { getServerSession } from 'next-auth';
-import { AUTHCONFIG } from './api/auth/[...nextauth]/route';
+import { options } from './api/auth/[...nextauth]/options';
 
 export default async function Home() {
-  const session = await getServerSession(AUTHCONFIG);
+  const session = await getServerSession(options);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       {session ? <Card user={session.user?.name} /> : <Form />}
